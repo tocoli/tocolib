dd861d99676b0d345b57b315f9692186f1be4c0b
swiesend <sebastian@wiesendahl.de>
2015-09-10 15:08:58 -0500

    changed benchmarking

--- tocoli/test.py
+++ tocoli/test.py
@@ -4,45 +4,73 @@
 # @copyright: © 2015 Sebastian Wiesendahl. All rights reserved.
 # @author: Sebastian Wiesendahl <sebastian@wiesendahl.de>
 
-import time
-import inspect
+from time import time
 
 
-def bench(function, args, rounds=1, collect=False, stopwatch=True, precision='0.3'):
-    """ Benchmark functions to test their performance. """
+def arguments(args=None, kwargs=None):
+    r = '('
 
-    res = []
-    start = 0.0
-    end = 0.0
-    r = range(rounds)
-    arg_count = len(inspect.getargspec(function).args)
-    if (arg_count > 1 and hasattr(args, '__iter__')):
-        start = time.time()
-        for i in r:
-            function(*args)
-        end = time.time()
+    if len(args) > 0:
+        r += ', '.join(tuple("'" + v + "'"
+            if isinstance(v, str) else
+        v.__class__.__name__ + ' #' + str(len(v))
+            if hasattr(v, '__iter__') and len(v) > 3 else
+        v.__class__.__name__
+            if hasattr(v, '__call__') else
+        str(v)
+            for v in args))
 
-        if collect:
-            for i in r:
-                res.append(function(*args))
-        else:
-            res = function(*args)
-    else:
-        start = time.time()
-        for i in r:
-            function(args)
-        end = time.time()
+    if len(kwargs) > 0:
+        if len(args) > 0:
+            r += ', '
+        r += ', '.join(tuple(str(k) + '=' + "'" + str(kwargs[k]) + "'"
+            if isinstance(kwargs[k], str) else
+        str(k) + '=' + kwargs[k].__class__.__name__ + ' #' + str(len(kwargs[k]))
+            if hasattr(kwargs[k], '__iter__') and len(kwargs[k]) > 3 else
+        str(k) + '=' + kwargs[k].__class__.__name__
+            if hasattr(kwargs[k], '__call__') else
+        str(k) + '=' + str(kwargs[k])
+            for k in sorted(kwargs)))
 
-        if collect:
-            for i in r:
-                res.append(function(args))
-        else:
-            res = function(args)
+    r += ')'
+    return r
 
-    if stopwatch:
-        print(('{:' + precision + 'f}').format(end - start) + 's')
 
-    return res
+class Bencher:
+
+    def __init__(self, rounds=1, collect=False, stopwatch=True, precision='0.8'):
+        self.rounds = rounds
+        self.collect = collect
+        self.stopwatch = stopwatch
+        self.precision = precision
+
+    def bench(self, function, *args, **kwargs):
+        res = []
+
+        benched = 0.0
+        r = range(self.rounds)
+
+        for _ in r:
+            start = time()
+            r = function(*args, **kwargs)
+            end = time()
+            benched += (end - start)
+
+            if self.collect:
+                res.append(r)
+            else:
+                res = r
+
+        if self.stopwatch:
+            print('benched: ' + function.__name__ + arguments(args, kwargs))
+            print('rounds:  ' + str(self.rounds))
+            print('average: ' + ('{:{}f}').format(benched / float(self.rounds), self.precision) + 's')
+            print('total:   ' + ('{:{}f}').format(benched, self.precision) + 's')
+            print('')
+
+        return res
+
+bench = Bencher().bench
 
 
 def fnprint(function, *args, **kwargs):
@@ -53,31 +81,8 @@ def fnprint(function, *args, **kwargs):
             s += sep
         return s
 
-    arguments = '('
-    if len(args) > 0:
-        arguments += ', '.join(tuple("'" + v + "'"
-                                        if isinstance(v, str) else
-                                     v.__class__.__name__ + ' #' + str(len(v))
-                                        if hasattr(v, '__iter__') and len(v) > 3 else
-                                     v.__class__.__name__
-                                        if hasattr(v, '__call__') else
-                                     str(v)
-                                        for v in args))
-    if len(kwargs) > 0:
-        if len(args) > 0:
-            arguments += ', '
-        arguments += ', '.join(tuple(str(k) + '=' + "'" + str(kwargs[k]) + "'"
-                                        if isinstance(kwargs[k], str) else
-                                     str(k) + '=' + kwargs[k].__class__.__name__ + ' #' + str(len(kwargs[k]))
-                                        if hasattr(kwargs[k], '__iter__') and len(kwargs[k]) > 3 else
-                                     str(k) + '=' + kwargs[k].__class__.__name__
-                                        if hasattr(kwargs[k], '__call__') else
-                                     str(k) + '=' + str(kwargs[k])
-                                        for k in sorted(kwargs)))
-    arguments += ')'
-
     print('')
-    print(sep('>>> ' + function.__name__ + arguments + ' '))
+    print(sep('>>> ' + function.__name__ + arguments(args, kwargs) + ' '))
     res = function(*args, **kwargs)
     pprint(res)
 
