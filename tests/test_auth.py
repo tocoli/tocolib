6925b2df5a5dcd74ef9134ca2889e642bd6c74db
swiesend <sebastian@wiesendahl.de>
2015-10-05 18:52:47 -0500

    added auth tests

---
+++ tests/test_auth.py
@@ -0,0 +1,66 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# @copyright: © 2015 Sebastian Wiesendahl. All rights reserved.
+# @author: Sebastian Wiesendahl <sebastian@wiesendahl.de>
+
+import unittest
+
+
+class Tests(unittest.TestCase):
+
+    # @unittest.skip("skip this test")
+    def test_generate_salt(self):
+        import string
+        from tocoli.auth import generate_salt, SALT_RANGE
+
+        salt = generate_salt()
+        self.assertTrue(all(s in SALT_RANGE for s in salt))
+        self.assertEqual(len(salt), 10)
+
+        salt = generate_salt(15)
+        self.assertTrue(all(s in SALT_RANGE for s in salt))
+        self.assertEqual(len(salt), 15)
+
+        salt = generate_salt(chars=string.ascii_lowercase)
+        self.assertTrue(all(s in string.ascii_lowercase for s in salt))
+        self.assertEqual(len(salt), 10)
+
+
+    def test_encrypt_password_and_verify_password(self):
+        from tocoli.auth import encrypt_password, verify_password
+
+        hash = encrypt_password('secret', rounds=1001)
+        elems = [h for h in hash.split('$') if len(h) > 0]
+        self.assertEqual(elems[0], '5')
+        self.assertEqual(elems[1], 'rounds=1001')
+        self.assertEqual(len(elems[2]), 16)
+        self.assertEqual(len(elems[3]), 43)
+        self.assertTrue(verify_password(hash, 'secret'))
+
+    def test_encrypt_token_and_verify_token(self):
+        from tocoli.auth import encrypt_token, verify_token
+
+        token = encrypt_token(u'Alice', u'secret', expiration=-1)
+        self.assertEqual(verify_token(token, u'secret'), None)
+
+        token = encrypt_token(u'Alice', u'secret') + 'modified'.encode('ascii')
+        self.assertEqual(verify_token(token, u'secret'), None)
+
+        token = encrypt_token(u'Alice', u'secret')
+        self.assertEqual(verify_token(token, u'secret'), u'Alice')
+
+    def test_encrypt_api_key_and_verify_api_key(self):
+        from tocoli.auth import encrypt_api_key, verify_api_key
+
+        api_key = encrypt_api_key(u'Alice', u'secret') + 'modified'.encode('ascii')
+        self.assertEqual(verify_api_key(api_key, u'secret'), None)
+
+        api_key = encrypt_api_key(u'Alice', u'secret')
+        self.assertEqual(verify_api_key(api_key, u'secret'), u'Alice')
+
+
+
+
+if __name__ == '__main__':
+    unittest.main()
--- tocoli/auth.py
+++ tocoli/auth.py
@@ -4,82 +4,61 @@
 # @copyright: © 2015 Sebastian Wiesendahl. All rights reserved.
 # @author: Sebastian Wiesendahl <sebastian@wiesendahl.de>
 
+import string
+import random
 from itsdangerous import TimedJSONWebSignatureSerializer as TimedSerializer, SignatureExpired
 from itsdangerous import JSONWebSignatureSerializer as Serializer, BadSignature
 from passlib.hash import sha256_crypt
-import string
-import random
 
+SALT_RANGE = string.ascii_uppercase + string.digits + string.ascii_lowercase
 
-def generate_salt(secret_key=None, length=10, chars=string.ascii_lowercase + string.digits):
+def generate_salt(length=10, chars=SALT_RANGE):
     '''Generates a salt. Defaults to a random salt from [a-z0-9].'''
-    salt = ''
-    if secret_key is None:
-        salt = ''.join(random.SystemRandom().choice(chars) for _ in range(length))
-    else:
-        for i in range(length):
-            try:
-                salt += secret_key[i]
-            except:
-                salt += '*'
-    return salt
+    r = random.SystemRandom()
+    return u''.join(r.choice(chars) for _ in range(length))
 
 
-class PasswordHandler:
-    '''Handle passwords'''
-
-    @staticmethod
-    def encrypt(password, rounds=4321):
-        '''Encrypts a password as sha256 hash'''
-        return sha256_crypt.encrypt(password, rounds=rounds)
-
-    @staticmethod
-    def verify(hash, password):
-        '''Verifies a password hash. Returns True on success else False'''
-        return sha256_crypt.verify(password, hash)
+def encrypt_password(password, rounds=100001):
+    '''Encrypts a password as hash.'''
+    return sha256_crypt.encrypt(password, rounds=rounds)
 
 
-class TokenHandler:
-    '''Handle tokens'''
-
-    def __init__(self, secret_key):
-        self.secret_key = secret_key
-
-    def encrypt(self, id, expiration=1800):
-        '''Encrypts a user id into a timed token. Default expiration time is 30 minutes.'''
-        ts = TimedSerializer(self.secret_key, expires_in=expiration)
-        return ts.dumps({ 'id': id })
-
-    def verify(self, token):
-        '''Verifies a given token. Returns the decoded user id or None.'''
-        ts = TimedSerializer(self.secret_key)
-        try:
-            data = ts.loads(token)
-        except SignatureExpired:
-            return None # valid token, but expired
-        except BadSignature:
-            return None # invalid token
-
-        return data['id'] if 'id' in data else None
+def verify_password(hash, password):
+    '''Verifies a password hash. Returns True on success else False'''
+    return sha256_crypt.verify(password, hash)
 
 
-class ApiKeyHandler:
-    '''Handle api keys'''
+def encrypt_token(user_id, secret_key, expiration=1800):
+    '''Encrypts a user id into a timed token. Default expiration time is 30 minutes.'''
+    ts = TimedSerializer(secret_key, expires_in=expiration)
+    return ts.dumps({ 'id': user_id })
 
-    def __init__(self, secret_key):
-        self.secret_key = secret_key
 
-    def encrypt(self, id):
-        '''Encrypts a user id into an api key (token). The generated api key has no expiration time.'''
-        s = Serializer(self.secret_key)
-        return s.dumps({'id': id, 'salt': generate_salt()})
+def verify_token(token, secret_key):
+    '''Verifies a given token. Returns the decoded user id or None.'''
+    ts = TimedSerializer(secret_key)
+    try:
+        data = ts.loads(token)
+    except SignatureExpired:
+        return None # valid token, but expired
+    except BadSignature:
+        return None # invalid token
 
-    def verify(self, token):
-        '''Verifies a given api key (token). Returns the decoded user id or None.'''
-        s = Serializer(self.secret_key)
-        try:
-            data = s.loads(token)
-        except BadSignature:
-            return None # invalid token
+    return data['id'] if 'id' in data else None
 
-        return data['id'] if 'id' in data else None
+
+def encrypt_api_key(user_id, secret_key):
+    '''Encrypts a user id into an api key (token). The generated api key has no expiration time.'''
+    s = Serializer(secret_key)
+    return s.dumps({'id': user_id, 'salt': generate_salt()})
+
+
+def verify_api_key(api_key, secret_key):
+    '''Verifies a given api key (token). Returns the decoded user id or None.'''
+    s = Serializer(secret_key)
+    try:
+        data = s.loads(api_key)
+    except BadSignature:
+        return None # invalid token
+
+    return data['id'] if 'id' in data else None
