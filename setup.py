e94bcca6a9c79a89031eae9fa4448710374bcaf5
Sebastian Wiesendahl <sebastian@wiesendahl.de>
2019-01-01 13:56:10 -0600

    add minimum dep for passlib

--- setup.py
+++ setup.py
@@ -83,7 +83,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=['six', 'chardet', 'python-levenshtein', 'itsdangerous', 'passlib'],
+    install_requires=['six', 'chardet', 'python-levenshtein', 'itsdangerous', 'passlib>=1.7.0'],
 
     # # List additional groups of dependencies here (e.g. development
     # # dependencies). You can install these using the following syntax,
